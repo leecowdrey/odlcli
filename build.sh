dbd7a7fc7eeed91a86e4def4683fd560586483af
Lee Cowdrey <lee@cowdrey.co.uk>
2021-05-19 18:34:23 +0100

    lee@cowdrey.co.uk: 1.0.4 build prep

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #git pull
 dpkg-deb --nocheck --build odlcli
-mv -f odlcli.deb dists/debian/amd64/odlcli-1.0.0_amd64.deb
-sudo alien --to-rpm dists/debian/amd64/odlcli-1.0.0_amd64.deb
+mv -f odlcli.deb dists/debian/amd64/odlcli-1.0.4_amd64.deb
+sudo alien --to-rpm dists/debian/amd64/odlcli-1.0.4_amd64.deb
 mv -f odlcli-1.0.?-?.noarch.rpm dists/rhel/noarch/
-#git add dists/debian/amd64/odlcli-1.0.0_amd64.deb
-#git add dists/rhel/noarch/odlcli-1.0.?-?.noarch.rpm
-#git commit -m "lee@cowdrey.co.uk: "
-#git push
+git add dists/debian/amd64/odlcli-1.0.4_amd64.deb
+git add dists/rhel/noarch/odlcli-1.0.?-?.noarch.rpm
+git commit -m "lee@cowdrey.co.uk: "
+git push
